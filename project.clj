b03d52531d018eadb5ea38a7d35b386470c5ae36
mwagg <michael@guerillatactics.co.uk>
2013-10-30 08:33:17 +0000

    Tutorial once. Simple clojurescript

--- project.clj
+++ project.clj
@@ -3,4 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :source-paths ["src/clj"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1978"]]
+  :plugins [[lein-cljsbuild "0.3.4"]]
+  :cljsbuild {:builds
+              [{:source-paths ["src/cljs"]
+                :compiler {
+                           :output-to "resources/public/js/modern.js"
+                           :optimizations :whitespace
+                           :pretty-print true}}]})
